765b51b017e26ab0069eecee5c5bccc41a17d1bf
Antje Sommer <antje.som@gmail.com>
2020-01-18 20:55:37 +0100

    change svg polygon in dropdown

--- components/LangSwitcher.vue
+++ components/LangSwitcher.vue
@@ -31,7 +31,7 @@ select {
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
-  background: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 150 50'><polygon fill='rgb(108, 92, 255)' points='0,0 100,0 50,50'/></svg>") no-repeat;
+  background: url("data:image/svg+xml, <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 136.059 90.71'><polygon fill='rgb(50, 170, 225)' points='0,22.678 90.707,22.678 45.353,68.032'/></svg>") no-repeat;
   background-size: 12px;
   background-position: calc(100% - 1rem) center;
   background-repeat: no-repeat;
