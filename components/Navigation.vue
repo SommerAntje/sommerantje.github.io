4464c268a7522e3708b05af2c931d52fc6ebd4db
Antje Sommer <antje.som@gmail.com>
2020-01-20 22:02:38 +0100

    cleanup navigation component

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,22 +1,58 @@
 <template>
-  <div class="nav">
-    <nav>
-        <nuxt-link to="/posts" class="ani">
-            Posts
-        </nuxt-link> |
-        <nuxt-link to="/resume" class="ani">
-            Resume
-        </nuxt-link>
-        <LangSwitcher/>
-    </nav>
-  </div>
+<div class="navigation-items">
+  <ul class="nav-list">
+    <li class="nav-item">
+      <nuxt-link to="/posts" class="ani">
+        <span> Posts </span>
+      </nuxt-link>
+    </li>
+    <li class="nav-item">
+    <nuxt-link to="/resume" class="ani">
+        <span> Resume </span>
+    </nuxt-link>
+    </li>
+  </ul>
+  <LangSwitcher />
+</div>
 </template>
 <script>
 import LangSwitcher from '~/components/LangSwitcher';
-
 export default {
   components: {LangSwitcher}
 }
 </script>
-<style lang="scss">
+<style scoped lang="scss">
+  .navigation-items {
+    @media (min-width: $screen-sm) {
+      display: -webkit-box;
+      display: flex;
+      -webkit-box-align: center;
+      align-items: center;
+      -webkit-box-pack: justify;
+      justify-content: space-between;
+    }
+  }
+
+  .nav-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    flex-direction: column; 
+    display: -webkit-box;
+    display: flex;  
+  }
+
+  .nav-item {
+    margin: 0 10px;
+  }
+
+  @media (min-width: $screen-sm) {
+    .nav-list {
+       -webkit-box-orient: horizontal;
+       -webkit-box-direction: normal;
+       flex-direction: row;
+    }
+  }
 </style>
