6ece4a6d3e7798a3ac3dca08089314abec3f55bb
Antje Sommer <antje.som@gmail.com>
2020-03-18 17:03:16 +0100

    use grid variables in hero component

--- components/Sections/HeroBanner.vue
+++ components/Sections/HeroBanner.vue
@@ -25,7 +25,6 @@
   </section>
 </template>
 
-
 <script>
 export default {};
 </script>
@@ -36,91 +35,97 @@ export default {};
   background: #fff;
   text-align: center;
   position: relative;
-}
-@media (min-width: 992px) {
-  .hero {
-    height: 488px;
+
+  @media (min-width: $screen-md) {
+    height: 488px; 
   }
-}
-@media (min-width: 768px) {
-  section {
+
+  @media (min-width: $screen-sm) {
     margin-bottom: 7.2rem;
   }
-}
-.hero__content {
-  width: 100%;
-  margin-right: auto;
-  margin-left: auto;
-  padding: 2.4rem;
-  max-width: 100%;
-  vertical-align: middle;
-}
 
-@media (min-width: 1140px) {
-  .hero__content {
-    max-width: 1000px;
-  }
-}
-@media (min-width: 992px) {
-  .hero__content {
-    max-width: 1000px;
-  }
-}
-@media (min-width: 768px) {
-  .hero__content {
-    max-width: 990px;
-  }
-}
-
-@media (min-width: 1140px) {
-  .hero__text {
-    width: 30%;
-  }
-}
-@media (min-width: 992px) {
-  .hero__text {
-    width: 40%;
-  }
-}
-@media (min-width: 768px) {
-  .hero__text {
-    width: 48%;
-    position: absolute;
-    min-height: 0;
-    margin-bottom: 0;
-  }
-}
-.hero__text {
-  position: absolute;
-  background: #fff;
-  min-height: 74px;
-  margin-bottom: 2rem;
-  padding: 1rem;
-  top: 55%;
-  width: 27%;
-  left: 21%;
-  @media (max-width: 375px) {
-    top: 68%;
-    width: 77%;
-    left: -6%;
-  }
-}
-
-.hero__profile {
-  margin-bottom: 0;
-  border: 5px solid #32aae1;
-  border-radius: 50%;
-  width: 50%;
-  position: relative;
-  @media (max-width: 375px) {
+  &__content {
     width: 100%;
-  }
-}
+    margin-right: auto;
+    margin-left: auto;
+    padding: 2.4rem;
+    max-width: 100%;
+    vertical-align: middle;
 
-img {
-  max-width: 100%;
-  height: auto;
-  -webkit-transition: all 0.3s ease;
-  transition: all 0.3s ease;
+    @media (min-width: $screen-lg) {
+     max-width: $container-lg;
+    }
+
+    @media (min-width: $screen-md) {
+     max-width: $container-lg;
+    }
+
+    @media (min-width: $screen-sm) {
+     max-width: $container-md;
+    }
+
+    img {
+      max-width: 100%;
+      height: auto;
+      -webkit-transition: all 0.3s ease;
+      transition: all 0.3s ease;
+    }
+  }
+
+  &__profile {
+    margin-bottom: 0;
+    border: 5px solid #32aae1;
+    border-radius: 50%;
+    width: 50%;
+    position: relative;
+
+    @media (max-width: $screen-xs) {
+      width: 100%;
+    }
+  }
+
+  &__text {
+    position: absolute;
+    background: #fff;
+    min-height: 74px;
+    margin-bottom: 2rem;
+    padding: 1rem;
+    top: 55%;
+    width: 27%;
+    left: 21%;
+
+    @media (min-width: $screen-lg) {
+      width: 30%;
+    }
+
+    @media (min-width: $screen-md) {
+      width: 40%;
+    }
+
+    @media (min-width: $screen-sm) {
+      width: 48%;
+      position: absolute;
+      min-height: 0;
+      margin-bottom: 0;
+    }
+
+    @media (max-width: $screen-xs) {
+      top: 68%;
+      width: 77%;
+      left: -6%;
+    }
+
+    h1 {
+      margin-bottom: 0;
+      padding: .7rem 2rem;
+      text-align: left;
+      -webkit-transition: background-color .3s ease-out;
+      transition: background-color .3s ease-out;
+    
+      @media (min-width: $screen-sm) {
+        font-size: 3rem;
+      }
+    }
+  }
 }
 </style>
