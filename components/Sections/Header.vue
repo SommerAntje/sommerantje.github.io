7a7de9e80f7b63463f5decd45346bcc0f2631aec
Antje Sommer <antje.som@gmail.com>
2020-04-02 21:55:46 +0200

    clean up breakpoint behavior in header

--- components/Sections/Header.vue
+++ components/Sections/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="the-header">
+  <header class="header">
     <div class="container">
-      <a href="/" class="the-header__link-logo nuxt-link-exact-active nuxt-link-active">
-         <img src="~/assets/images/logo-antje.svg" alt="Logo Antje Sommer" class="the-header__logo" />
+      <a href="/" class="header__link-logo nuxt-link-exact-active nuxt-link-active">
+         <img src="~/assets/images/logo-antje.svg" alt="Logo Antje Sommer" class="header__logo" />
       </a>
       <DrawerToggle/>
       <Navigation/>
@@ -21,7 +21,7 @@ export default {
 </script>
 
 <style lang="scss">
-.the-header {
+.header {
   width: 100%;
   height: 6rem;
   display: -webkit-box;
@@ -31,41 +31,52 @@ export default {
   align-items: center;
   z-index: 100;
   box-shadow: 0 4px 12px 0 rgba(0,0,0,.05);
-  background-color: #fff;
+  background-color: $background-primary;
+  
+  @media (min-width: $screen-sm){
+  	height: 8rem;
+  }
 
   &__logo {
     height: 33px;
   }
 
   &__link-logo {
-     line-height: 0;
+    line-height: 0;
   }
 
   .container {
-    display: -webkit-box;
+  	display: -webkit-box;
     display: flex;
     -webkit-box-align: center;
     align-items: center;
     -webkit-box-pack: justify;
     justify-content: space-between;
-     width: 100%;
+    width: 100%;
 
-    @media (max-width: $screen-xs){
+    @media(max-width: $screen-xs){
+      flex-direction: row;
+    }
+
+    @media(max-width: $screen-sm){
       flex-direction: row;
     }
   }
-  @media (min-width: $screen-sm) {
-    .drawer-toggle {
-        display: none;
+
+  .navigation-items {
+  	@media (max-width: $screen-xs) {
+  		display: none;
     }
-  }
-  @media (min-width: $screen-sm) {
-    height: 8rem;
-  }
-  @media (max-width: $screen-xs) {
-    .navigation-items {
+    
+    @media(max-width: $screen-sm){
       display: none;
     }
+  }
+
+  .drawer-toggle {
+  	@media(min-width: $screen-sm) {
+  		display: none;
+  	}
   }
 }
 </style>
