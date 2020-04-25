2ce387d427b036d24e180a6bf861c4dfd7d54fe2
Antje Sommer <antje.som@gmail.com>
2020-04-25 19:45:09 +0200

    set basic structurefor timeline layout

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -1,5 +1,110 @@
 <template>
-    <p> timeline goes here</p>
+    <div class="timeline">
+        <ul>
+            <li>
+                <div class="timeline__content">
+                    <h3>first entry</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
+                        Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum. 
+                        Id donec ultrices tincidunt arcu. Porta nibh venenatis cras sed. Interdum velit euismod in pellentesque
+                        massa placerat duis. Id diam maecenas ultricies mi. Pretium nibh ipsum consequat nisl vel pretium. 
+                        Enim diam vulputate ut pharetra 
+                        sit amet aliquam id diam. Arcu felis bibendum ut tristique et. Vel eros donec ac odio tempor.
+                    </p>
+                </div>
+                <div class="timeline__time">
+                    <h4>January 2018</h4>
+                </div>
+            </li>
+             <li>
+                <div class="timeline__content">
+                    <h3>first entry</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
+                        Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum. 
+                        Id donec ultrices tincidunt arcu. Porta nibh venenatis cras sed. Interdum velit euismod in pellentesque
+                        massa placerat duis. Id diam maecenas ultricies mi. Pretium nibh ipsum consequat nisl vel pretium. 
+                        Enim diam vulputate ut pharetra 
+                        sit amet aliquam id diam. Arcu felis bibendum ut tristique et. Vel eros donec ac odio tempor.
+                    </p>
+                </div>
+                <div class="timeline__time">
+                    <h4>January 2018</h4>
+                </div>
+            </li>
+             <li>
+                <div class="timeline__content">
+                    <h3>first entry</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
+                        Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum. 
+                        Id donec ultrices tincidunt arcu. Porta nibh venenatis cras sed. Interdum velit euismod in pellentesque
+                        massa placerat duis. Id diam maecenas ultricies mi. Pretium nibh ipsum consequat nisl vel pretium. 
+                        Enim diam vulputate ut pharetra 
+                        sit amet aliquam id diam. Arcu felis bibendum ut tristique et. Vel eros donec ac odio tempor.
+                    </p>
+                </div>
+                <div class="timeline__time">
+                    <h4>January 2018</h4>
+                </div>
+            </li>
+             <li>
+                <div class="timeline__content">
+                    <h3>first entry</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
+                        Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum. 
+                        Id donec ultrices tincidunt arcu. Porta nibh venenatis cras sed. Interdum velit euismod in pellentesque
+                        massa placerat duis. Id diam maecenas ultricies mi. Pretium nibh ipsum consequat nisl vel pretium. 
+                        Enim diam vulputate ut pharetra 
+                        sit amet aliquam id diam. Arcu felis bibendum ut tristique et. Vel eros donec ac odio tempor.
+                    </p>
+                </div>
+                <div class="timeline__time">
+                    <h4>January 2018</h4>
+                </div>
+            </li>
+             <li>
+                <div class="timeline__content">
+                    <h3>first entry</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
+                        Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum. 
+                        Id donec ultrices tincidunt arcu. Porta nibh venenatis cras sed. Interdum velit euismod in pellentesque
+                        massa placerat duis. Id diam maecenas ultricies mi. Pretium nibh ipsum consequat nisl vel pretium. 
+                        Enim diam vulputate ut pharetra 
+                        sit amet aliquam id diam. Arcu felis bibendum ut tristique et. Vel eros donec ac odio tempor.
+                    </p>
+                </div>
+                <div class="timeline__time">
+                    <h4>January 2018</h4>
+                </div>
+            </li>
+             <li>
+                <div class="timeline__content">
+                    <h3>first entry</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
+                        Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum. 
+                        Id donec ultrices tincidunt arcu. Porta nibh venenatis cras sed. Interdum velit euismod in pellentesque
+                        massa placerat duis. Id diam maecenas ultricies mi. Pretium nibh ipsum consequat nisl vel pretium. 
+                        Enim diam vulputate ut pharetra 
+                        sit amet aliquam id diam. Arcu felis bibendum ut tristique et. Vel eros donec ac odio tempor.
+                    </p>
+                </div>
+                <div class="timeline__time">
+                    <h4>January 2018</h4>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
 export default {
