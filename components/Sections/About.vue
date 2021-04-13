280ebaa9727fe7c5e15f887f55f3b9a723a89c05
Antje Sommer <antje.som@gmail.com>
2021-04-13 20:33:56 +0200

    add content to about me section

--- components/Sections/About.vue
+++ components/Sections/About.vue
@@ -1,9 +1,11 @@
 <template>
     <section>
        <p class="about">
-        I am Antje, a passionate Frontend Developer with longtime experience 
-        in UI/UX Design and Ecommerce, based in Hamburg.
-        Currently diving into the magical world of Vue.js and Typescript while working remotely for dpa-IT!
+        Hello and welcome to my tech blog! <br /> I am Antje a graphic designer, autodidact and Front-End Developer with passion. 
+        <br /> Taking online shop development to the next level, I've been working as UI/UX Designer and Front-End Developer in E-Commerce for many years.
+         Following my passion for constant learning and upgrading my front end skills, I am currently working in a scrum team at the dpa-IT Service GmbH
+        where I am diving into the magical world of Vue.js and Typescript.
+        Apart from that I am also an autodidactic guitarist (bloody beginner level) and enthusiastic about learning dutch!
       </p>
       <Timeline />
     </section>
