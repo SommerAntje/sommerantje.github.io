e5ddef3052b6d9df3da71b47c5e91aa3d0a35759
Antje Sommer <antje.som@gmail.com>
2021-04-13 22:08:00 +0200

    add content  to timeline

--- components/Sections/About.vue
+++ components/Sections/About.vue
@@ -3,7 +3,7 @@
        <p class="about">
         Hello and welcome to my tech blog! <br /> I am Antje a graphic designer, autodidact and Front-End Developer with passion. 
         <br /> Taking online shop development to the next level, I've been working as UI/UX Designer and Front-End Developer in E-Commerce for many years.
-         Following my passion for constant learning and upgrading my front end skills, I am currently working in a scrum team at the dpa-IT Service GmbH
+         Following my passion for constant learning and upgrading my front end skills, I am currently working in a scrum team at the dpa-IT Services GmbH
         where I am diving into the magical world of Vue.js and Typescript.
         Apart from that I am also an autodidactic guitarist (bloody beginner level) and enthusiastic about learning dutch!
       </p>
--- components/Timeline.vue
+++ components/Timeline.vue
@@ -3,80 +3,99 @@
         <ul>
             <li>
                 <div class="timeline__content">
-                    <h3>first entry</h3>
+                    <h3>dpa-IT Services GmbH</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
-                        Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum. 
+                        At the moment I am working as Front-End Engineer at the dpa-IT Services GmbH
+                        where I am diving into the magical world of Vue.js, Typescript, TailwindCSS
+                        and sharp my testing skills in vue-testing-library.<br />
+                        <span class="highlight">#vue.js</span>
+                        <span class="highlight">#Tailwind CSS</span> <br />
+                        <span class="highlight">#Typescript</span>
+                        <span class="highlight">#GraphQL</span>
+                        <span class="highlight">#vue-testing-library</span>
                     </p>
                 </div>
                 <div class="timeline__time">
-                    <h4>January 2018</h4>
+                    <h4>May 2020 - now</h4>
                 </div>
             </li>
              <li>
                 <div class="timeline__content">
-                    <h3>first entry</h3>
+                    <h3>dreamIT GmbH</h3>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
                         Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum.
+                        <br />
+                        <span class="highlight">#vue.js</span>
+                        <span class="highlight">#vuetify</span>
+                        <span class="highlight">#Micro Frontends</span><br />
+                        <span class="highlight">#GraphQL</span>
+                        <span class="highlight">#vue-testing-library</span>
                     </p>
                 </div>
                 <div class="timeline__time">
-                    <h4>January 2018</h4>
+                    <h4>Nov' 2018 - Apr' 2020</h4>
                 </div>
             </li>
              <li>
                 <div class="timeline__content">
-                    <h3>first entry</h3>
+                    <h3>avarto eCommerce Verwaltungsgesellschaft mbH</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        Software Front-End Engineer | Salesforce Commerce Cloud
+                        for  Uniqlo, Gerry Weber, Tommy Hilfiger.
                         labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
                         Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum.
+                        <br />
+                        <span class="highlight">#Salesforce Commerce Cloud</span>
+                        <span class="highlight">#jQuery #ES6</span><br />
+                        <span class="highlight">#E-Commerce</span>
+                        <span class="highlight">#HTML5</span>
+                        <span class="highlight">#CSS3</span>
                     </p>
                 </div>
                 <div class="timeline__time">
-                    <h4>January 2018</h4>
+                    <h4>May' 2015 - Oct' 2018</h4>
                 </div>
             </li>
              <li>
                 <div class="timeline__content">
-                    <h3>first entry</h3>
+                    <h3>ePages GmbH</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        Software Front-End Engineer | E-commerce, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
                         Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum.
+                        <br />
+                        <span class="highlight">#Bootstrap</span>
+                        <span class="highlight">#jQuery</span>
+                        <span class="highlight">#ePages Template Framework</span><br />
+                        <span class="highlight">#CSS3</span>
+                        <span class="highlight">#HTML5</span>
+                        <span class="highlight">#E-Commerce</span>
                     </p>
                 </div>
                 <div class="timeline__time">
-                    <h4>January 2018</h4>
+                    <h4>Oct' 2013 - Apr' 2015</h4>
                 </div>
             </li>
              <li>
                 <div class="timeline__content">
-                    <h3>first entry</h3>
+                    <h3>Ministry Group GmbH</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        Trainee in web development with focus on frontend, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
                         Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum.
+                        <br />
+                        <span class="highlight">#Bootstrap</span>
+                        <span class="highlight">#jQuery</span>
+                        <span class="highlight">#PHP</span><br />
+                        <span class="highlight">#HTML5</span>
+                        <span class="highlight">#CSS3</span>
+                        <span class="highlight">#E-Commerce</span>
                     </p>
                 </div>
                 <div class="timeline__time">
-                    <h4>January 2018</h4>
-                </div>
-            </li>
-             <li>
-                <div class="timeline__content">
-                    <h3>first entry</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac habitasse platea.
-                        Ornare lectus sit amet est placerat in egestas. Sem fringilla ut morbi tincidunt augue interdum.
-                    </p>
-                </div>
-                <div class="timeline__time">
-                    <h4>January 2012</h4>
+                    <h4>Feb' 2013 - Aug' 2013</h4>
                 </div>
             </li>
             <div class="timeline__clear-both"></div>
@@ -160,7 +179,7 @@ export default {
                 .timeline__time {
                     position: absolute;
                     top: 12px;
-                    right: -150px;
+                    right: -190px;
                     margin: 0;
                     padding: 8px 16px;
                     background: rgba(53,168,224,1);
@@ -203,7 +222,7 @@ export default {
                 .timeline__time {
                     position: absolute;
                     top: 12px;
-                    left: -150px;
+                    left: -190px;
                     margin: 0;
                     padding: 8px 16px;
                     background: rgba(53,168,224,1);
@@ -237,6 +256,12 @@ export default {
             font-family: 'Raleway ExtraLight', Arial, sans-serif;
             color: rgba(53,168,224,1);
             font-size: 1.75rem;
+        }
+        .highlight {
+            font-family: 'Raleway ExtraLight', Arial, sans-serif;
+            color: rgba(53,168,224,1);
+            line-height: 1.5;
+            letter-spacing: -.1px;
         }
         p {
             margin: 10px 0 0;
