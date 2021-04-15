5089d38bb4a64989f7f4b72b6f1797a3764b12dd
Antje Sommer <antje.som@gmail.com>
2021-04-15 20:43:32 +0200

    update credits

--- locales/de/footer.js
+++ locales/de/footer.js
@@ -1,3 +1,3 @@
 export default {
-  credits: 'Website and content by <a target="_blank" href="https://antje-sommer.de/" class="ani"><span>Antje Sommer</span></a>. Blog based on an open source template, checkout my GitHub profile <a target="_blank" href="https://github.com/SommerAntje" class="ani"><span>Github</span></a>.'
+  credits: 'Website and content by <a target="_blank" href="https://antje-sommer.de" class="ani"><span>Antje Sommer</span></a>. Blog based on an open source template, checkout my <a target="_blank" href="https://github.com/SommerAntje" class="ani"><span>Github</span></a> profile.'
 }
