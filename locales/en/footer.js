f649302c8062f9c45c082a81af8ac82763da8a25
Antje Sommer <antje.som@gmail.com>
2021-05-14 17:22:05 +0200

    rephrase footer credits

--- locales/en/footer.js
+++ locales/en/footer.js
@@ -1,3 +1,3 @@
 export default {
-  credits: 'Website and content built in nuxt + vue.js - by <a target="_blank" href="https://github.com/SommerAntje" class="ani"><span>Antje Sommer</span></a>. Blog based on open source template.'
+  credits: 'Website and content built in nuxt + vue.js + markdown - by <a target="_blank" href="https://github.com/SommerAntje" class="ani"><span>Antje Sommer</span></a>. Blog based on open source template.'
 }
